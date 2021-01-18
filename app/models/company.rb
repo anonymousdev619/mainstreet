7610622e0791d620b9a93483618dc29611545942
Manoj Naidu <manojnaidu619@gmail.com>
2021-01-18 20:06:28 +0530

    email DB validation done

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,14 @@
 class Company < ApplicationRecord
   has_rich_text :description
+  validates :email, presence: true, uniqueness: true
+  validate :custom_email_validation
 
+  private
 
+  def custom_email_validation
+    base_domain_string = "getmainstreet.com"
+    index = email.index("@")
+    domain_string = email[(index+1)..-1] 
+    errors.add(:email, "email is not valid!") if domain_string != base_domain_string
+  end
 end
